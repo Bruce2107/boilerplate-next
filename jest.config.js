1cfbca977890148f8cb25260956de6757271ea02
Eduardo Henrique <eduardo.henrique@zup.com.br>
2020-11-26 16:29:46 -0300

    fix: jest coverage

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.test.ts(x)'],
+  collectCoverageFrom: ['src/**/*.test.ts(x)?'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/']
 }
