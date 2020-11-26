1bbf547582fb3ba971e723132cc47c29d5479462
Eduardo Henrique <eduardo.henrique@zup.com.br>
2020-11-26 17:11:58 -0300

    chore: adiciona generators

---
+++ generators/plopfile.js
@@ -0,0 +1,41 @@
+module.exports = function (plop) {
+  plop.setGenerator('component', {
+    description: 'application component logic',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'component name please'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/index.tsx',
+        templateFile: 'templates/index.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/styles.tsx',
+        templateFile: 'templates/styles.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path:
+          '../src/components/{{pascalCase name}}/{{camelCase name}}.stories.tsx',
+        templateFile: 'templates/stories.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path:
+          '../src/components/{{pascalCase name}}/{{camelCase name}}.test.tsx',
+        templateFile: 'templates/test.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/types.tsx',
+        templateFile: 'templates/types.ts.hbs'
+      }
+    ]
+  })
+}
