e3ed18ecbb1f3f9c80fcc63580b09bd43e616da4
Eduardo Henrique <eduardo.henrique@zup.com.br>
2020-11-27 14:09:27 -0300

    chore: generic files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>Boilerplate</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
