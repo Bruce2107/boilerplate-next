fd68dd8f7ff7f67653e2548642322e92713a6522
Eduardo Henrique <eduardo.henrique@zup.com.br>
2021-01-04 17:03:27 -0300

    feat: adiciona tema

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,8 @@
 module.exports = {
-  stories: [
-    '../src/components/**/*.stories.(md|ts)x',
-  ],
-  addons: ['@storybook/addon-essentials']
+  stories: ['../src/components/**/*.stories.@(md|ts)x'],
+  addons: ['@storybook/addon-essentials'],
+  webpackFinal: (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`)
+    return config
+  }
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, css } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
   @font-face {
@@ -6,26 +6,24 @@ const GlobalStyles = createGlobalStyle`
     font-style: normal;
     font-weight: 300;
     font-display: swap;
-    src: local(''),
-        url('/fonts/poppins-v15-latin-300.woff2') format('woff2'),
+    src: local('Poppins Light'), local('Poppins-Light'),
+        url('/fonts/poppins-v15-latin-300.woff2') format('woff2');
   }
-
   @font-face {
     font-family: 'Poppins';
     font-style: normal;
     font-weight: 400;
     font-display: swap;
-    src: local(''),
-        url('/fonts/poppins-v15-latin-regular.woff2') format('woff2'),
+    src: local('Poppins Regular'), local('Poppins-Regular'),
+        url('/fonts/poppins-v15-latin-regular.woff2') format('woff2');
   }
-
   @font-face {
     font-family: 'Poppins';
     font-style: normal;
     font-weight: 600;
     font-display: swap;
-    src: local(''),
-        url('/fonts/poppins-v15-latin-600.woff2') format('woff2'),
+    src: local('Poppins SemiBold'), local('Poppins-SemiBold'),
+        url('/fonts/poppins-v15-latin-600.woff2') format('woff2');
   }
 
   * {
@@ -35,15 +33,22 @@ const GlobalStyles = createGlobalStyle`
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
-  html {
-    font-size: 62.5%;
-  }
-  html, body, #__next {
-    height: 100%;
-  }
-  body {
-    font-family: 'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
-  }
+
+  ${({ theme }) => css`
+    html {
+      font-size: 62.5%;
+    }
+    html,
+    body,
+    #__next {
+      height: 100%;
+    }
+    body {
+      font-family: ${theme.font.family};
+      font-size: ${theme.font.sizes.medium};
+    }
+  `}
+
 `
 
 export default GlobalStyles
